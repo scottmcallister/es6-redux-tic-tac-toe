0049057b5330e2f1ba914fbe9f6488c4567dafec
Scott McAllister <mr.scott.mcallister@gmail.com>
2017-02-13 09:42:34 -0500

    add check for game winner

--- js/game.js
+++ js/game.js
@@ -1,5 +1,51 @@
 import * as actions from './actions';
 
+// all possible 3 in a row coordinates for each box in grid
+const winConditions = [
+    [
+        [[0,0],[0,1],[0,2]],
+        [[0,0],[1,1],[2,2]],
+        [[0,0],[1,0],[2,0]]
+    ],
+    [
+        [[0,0],[0,1],[0,2]],
+        [[0,1],[1,1],[2,1]]
+    ],
+    [
+        [[0,0],[0,1],[0,2]],
+        [[2,0],[1,1],[0,2]],
+        [[2,2],[1,2],[0,2]]
+    ],
+    [
+        [[0,0],[1,0],[2,0]],
+        [[1,0],[1,1],[1,2]]
+    ],
+    [
+        [[0,0],[1,1],[2,2]],
+        [[1,0],[1,1],[1,2]],
+        [[0,1],[1,1],[2,1]],
+        [[2,0],[1,1],[0,2]]
+    ],
+    [
+        [[0,2],[1,2],[2,2]],
+        [[1,0],[1,1],[1,2]]
+    ],
+    [
+        [[0,0],[0,1],[0,2]],
+        [[0,2],[1,1],[2,0]],
+        [[0,2],[1,2],[2,2]]
+    ],
+    [
+        [[0,1],[1,1],[2,1]],
+        [[2,0],[2,1],[2,2]]
+    ],
+    [
+        [[2,0],[2,1],[2,2]],
+        [[0,0],[1,1],[2,2]],
+        [[0,2],[1,2],[2,2]]
+    ]
+];
+
 class Game {
     constructor(options) {
         this.ui = options.ui;
@@ -26,6 +72,19 @@ class Game {
             const value = grid[Math.floor(index/3)][index % 3];
             box.innerHTML = value;
         });
+    }
+
+    isWinner(row, col, player) {
+        let gameOver = false;
+        const boxIndex = Math.floor(row/3) + (col % 3);
+        winConditions[boxIndex].map((wc) => {
+            // check if win condition coordinates match player
+            gameOver =
+                this.ui.grid[wc[0][0]][wc[0][1]] === player &&
+                this.ui.grid[wc[1][0]][wc[1][1]] === player &&
+                this.ui.grid[wc[2][0]][wc[2][1]] === player;
+        });
+        return gameOver;
     }
 
     resetGame() {
